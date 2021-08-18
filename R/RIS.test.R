2288f8a2bdd856b19e8e3f6dcd973f4def597cf1
SRingshandl <89140097+SRingshandl@users.noreply.github.com>
2021-08-18 15:53:12 +0200

    Add files via upload

---
+++ R/RIS.test.R
@@ -0,0 +1,14 @@
+#' This is a function to test functionality
+#'
+#' Write RIS.test and see whether you get an output.
+#' If yes, the package ist installed correctly.
+#' @keywords test
+#' @export
+#' @examples
+#' RIS.test()
+
+
+RIS.test <- function() {
+print("Hi there!")
+print("This is your requested test output! RISpackage is installed correctly!")
+}
