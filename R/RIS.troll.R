3f7b072a268a5df980e02dacdc36422518ead837
SRingshandl <89140097+SRingshandl@users.noreply.github.com>
2021-08-19 10:10:51 +0200

    Add files via upload

---
+++ R/RIS.troll.R
@@ -0,0 +1,13 @@
+#' Need some refreshment?
+#'
+#' This function allows you to ease up.
+#' @keywords RISpackage
+#' @keywords rickroll
+#' @export
+#' @examples
+#' RIS.troll()
+
+
+RIS.troll <- function() {
+  browseURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ&mode=theatre")
+}
